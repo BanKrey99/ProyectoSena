d4e0ec1c9cbcc7ddb833dbbd42a461b1bb5313b6
Andrey Medina <bankrey08@gmail.com>
2025-10-02 08:36:22 -0500

    Se agregaron rutas de Tipo de Inmueble

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\TipoInmuebleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,11 @@ Route::post('/municipio/store', [MunicipioController::class, 'store'])->name('mu
 Route::get('/municipio/edit/{id}', [MunicipioController::class, 'edit'])->name('municipios.edit');
 Route::post('/municipio/update/{id}', [MunicipioController::class, 'update'])->name('municipios.update');
 Route::post('/municipio/destroy/{id}', [MunicipioController::class, 'destroy'])->name('municipios.destroy');
+
+//Rutas de Tipo de Inmueble 
+Route::get('/tipoInmueble/index', [TipoInmuebleController::class, 'index'])->name('tipoInmueble.index');
+Route::get('/tipoInmueble/create', [TipoInmuebleController::class, 'create'])->name('tipoInmueble.create');
+Route::post('/tipoInmueble/store', [TipoInmuebleController::class, 'store'])->name('tipoInmueble.store');
+Route::get('/tipoInmueble/edit/{id}', [TipoInmuebleController::class, 'edit'])->name('tipoInmueble.edit');
+Route::post('/tipoInmueble/update/{id}', [TipoInmuebleController::class, 'update'])->name('tipoInmueble.update');
+Route::post('/tipoInmueble/destroy/{id}', [TipoInmuebleController::class, 'destroy'])->name('tipoInmueble.destroy');
