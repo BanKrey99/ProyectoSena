eb96e864fb9869170a5bb318ed7a16d9d35ef767
felipeortiz1 <za09099az@gmail.com>
2025-10-03 11:00:37 -0500

    Se creo el index

---
+++ resources/views/Barrios/index.blade.php
@@ -0,0 +1,82 @@
+@extends('layout.app')
+
+
+@section('title')
+Barrios
+@endsection
+
+@section('titleContent')
+Administrar Barrios
+@endsection
+
+@section('content')
+<div>
+    <a href="{{ route('barrios.create') }}" class="btn btn-primary mb-3"> + Crear Barrio </a>
+    <div>
+        <table class="table table-bordered text-center align-middle table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($barrios as $barrio)
+                <tr>
+                    <td>{{$barrio->id}}</td>
+                    <td>{{$barrio->nombre}}</td>
+                    <td>
+                        <a href="{{ route('barrios.edit', $barrio->id) }}" class="btn btn-warning">Actualizar</a>
+                        <form action="{{ route('barrios.destroy', $barrio->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            <button type="submit" class="btn btn-danger" onclick="confirmarEliminacion(event)">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="col-12 text-center my-3">
+            <a href="{{ route('welcome') }}" class="btn btn-secondary text-center mt-3">Volver</a>
+        </div>
+    </div>
+</div>
+@endsection
+
+@if(session('success'))
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        Swal.fire({
+            icon: 'success',
+            title: '¡Éxito!',
+            text: "{{ session('success') }}",
+            confirmButtonText: 'Aceptar',
+            timer: 10000
+        });
+    });
+</script>
+@endif
+
+<script>
+    function confirmarEliminacion(event) {
+        event.preventDefault();
+        const form = event.target.closest('form');
+
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "¡No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit();
+            }
+        });
+    }
+</script>
