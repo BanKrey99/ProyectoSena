89027e188b699baa24a8295e6b8e0bd6ed87c44a
Andrey Medina <bankrey08@gmail.com>
2025-10-10 08:24:50 -0500

    Se modifico el modelo para relacionar Muncipio con Barrios

--- app/Models/Barrio.php
+++ app/Models/Barrio.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Barrio extends Model
 {
     protected $table = 'barrios';
-    protected $fillable = ['nombre'];
+    protected $fillable = ['nombre','idMunicipio'];
+
+    public function Municipios()
+    {
+        return $this->belongsTo(Municipio::class, 'idMunicipio');
+    }
 }
--- app/Models/Municipio.php
+++ app/Models/Municipio.php
@@ -12,4 +12,9 @@ class Municipio extends Model
         'nombre',
         'codigoPostal'
     ];
+
+    public function Barrio()
+    {
+        return $this->hasMany(Barrio::class);
+    }
 }
